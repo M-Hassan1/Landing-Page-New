8dd61c0d9d8a2b4e9891e896b320f95790c19d57
M-Hassan1 <hassan81932@gmail.com>
2023-10-07 12:46:50 +0500

    exported Topbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import TopBar from '@/components/view/Header/Navbar/TopBar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      
+      <TopBar />
     </div>
   )
 }
