615a5876d85c6a166a7c84c356c515d2cb3b4ec1
M-Hassan1 <hassan81932@gmail.com>
2023-10-08 06:17:49 +0500

    pushed

--- src/components/view/BrowsReviews/index.tsx
+++ src/components/view/BrowsReviews/index.tsx
@@ -1,32 +1,26 @@
-
 import React from 'react';
 import CardPage from './CardPage';
 import { cardsData } from '@/components/utils/arrays';
 
 const BrowsReviews: React.FC = () => {
   return (
-    <div className='text-center pt-32 pb-16 '>
-        <h1 className='text-4xl text-bold font-bold'>Brows Reviews</h1>
-    <div className="flex justify-center items-center pt-4 ">
-      <div className="grid grid-cols-4 gap-4">
-        {cardsData.map((data, index) => (
-          <CardPage
-            key={index}
-            profileIcon={data.profileIcon}
-            name={data.name}
-            rating={data.rating}
-            description={data.description}
-          />
-        ))}
-      </div>
+    <div className='text-center pt-8 pb-16'>
+      <h1 className='text-4xl font-bold'>Brows Reviews</h1>
+      <div className="container mx-auto mt-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {cardsData.map((data, index) => (
+            <CardPage
+              key={index}
+              profileIcon={data.profileIcon}
+              name={data.name}
+              rating={data.rating}
+              description={data.description}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default BrowsReviews;
-
-
-
-
-
