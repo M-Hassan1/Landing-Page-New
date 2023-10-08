8841ad0d0afb2aa4c756bb5d288082b6ac5f5ef3
M-Hassan1 <hassan81932@gmail.com>
2023-10-08 10:18:16 +0500

    Solved Error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import BrowsReviews from "@/components/view/BrowsReviews";
 import Category from "@/components/view/Category/Category";
-import Footer from "@/components/view/Footer/FooterFile/Footer";
-import HomePage from "@/components/view/HeroSection";
 import LatestInternships from "@/components/view/InternShips";
-import NavBar from "@/components/view/Navabar";
 import CoursesOffer from "@/components/view/WhatWeOffer";
 
 
